8da862796e3f8e4aa2c1b4d0f4458940d54588e6
David Eko <david.putra@mcpayment.co.id>
2022-08-28 01:04:10 +0700

    Fix

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    host: 'localhost',
   });
 
   server.route(routes);
